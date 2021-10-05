1f2a45f4df0ca9b19dcccb3e4713248d8cdb7b21
FranciscoMorenoSantaella <santaellamorenofrancisco@gmail.com>
2021-10-05 09:22:49 +0200

    requires jaxb in module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.EternityChat {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.xml.bind;
 
     opens org.EternityChat to javafx.fxml;
     exports org.EternityChat;
