0c3cb0549336c36801aa69ea0febc94d34488c5a
AngelRabasco <82035161+AngelRabasco@users.noreply.github.com>
2021-10-05 10:10:27 +0200

    App now launches the login screen.

--- src/main/java/org/EternityChat/App.java
+++ src/main/java/org/EternityChat/App.java
@@ -17,8 +17,10 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("Login"));
         stage.setScene(scene);
+        stage.setTitle("Eternity Chat LogIn");
+        stage.setResizable(false);
         stage.show();
     }
 
