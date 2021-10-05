3db9b7be34187878ba18a2242adc66299e80aaf6
AngelRabasco <82035161+AngelRabasco@users.noreply.github.com>
2021-10-05 10:14:35 +0200

    Fixed JAXB

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.EternityChat {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.xml.bind;
+    requires com.sun.xml.bind;
 
     opens org.EternityChat to javafx.fxml;
     exports org.EternityChat;
