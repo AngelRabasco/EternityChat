c1431ceb610f25256be6ca72545d341ed944754c
FranciscoMorenoSantaella <82145481+FranciscoMorenoSantaella@users.noreply.github.com>
2021-10-08 09:42:43 +0200

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,8 +3,11 @@ module org.EternityChat {
     requires javafx.fxml;
     requires java.xml.bind;
     requires com.sun.xml.bind;
-		requires javafx.graphics;
+    requires javafx.graphics;
 
-    opens org.EternityChat to javafx.fxml;
+    opens org.EternityChat to javafx.fxml, java.xml.bind, javafx.base;
+    opens org.EternityChat.Model to javafx.xml, java.xml.bind;
+    opens org.EternityChat.Util to com.sun.xml.bind;
+	
     exports org.EternityChat;
 }
